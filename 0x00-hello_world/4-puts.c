96782f30e348d5861b88a668d642a458cadf4279
Cyprian <cyprianmutai5900@gmail.com>
2023-02-16 01:38:02 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Ebtry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/*prints out text*/
+	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
