83e75de88b550137a5bd41d472fee9b28f537820
Cyprian <cyprianmutai5900@gmail.com>
2023-02-17 04:11:33 -0800

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 9; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = a + 1; b < 10; b++)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-			if (a != 8 || b != 9)
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a != 8 && b != 9)
 			{
 				putchar(',');
 				putchar(' ');
