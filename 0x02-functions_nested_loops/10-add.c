7f96aa68144d257bcfb0c87609dcd3e822e5e083
Cyprian <cyprianmutai5900@gmail.com>
2023-02-21 08:28:09 -0800

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ *@a: integer
+ *@b: integer
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+	_putchar('\n');
+}
