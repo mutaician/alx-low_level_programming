dd781147adfa4a65c66a37eaf90c75c13e61962b
Cyprian <cyprianmutai5900@gmail.com>
2023-02-21 06:34:55 -0800

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  */
 void print_alphabet_x10(void)
 {
-	int i, n;
+	int n, i;
 
 	for (n = 0; n <= 9; n++)
 	{
@@ -14,6 +14,5 @@ void print_alphabet_x10(void)
 		{
 			_putchar(i);
 		}
-	}
-	_putchar('\n');
+		_putchar('\n');
 }
