9ca09be9108dc0bd19146821a33d2f53c7ef8812
Cyprian <cyprianmutai5900@gmail.com>
2023-02-21 08:43:12 -0800

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers to 98
+ *
+ * @n: integer
+ * return: 0 ALways
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			putchar(n);
+			n = n + 1;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			putchar(n);
+			n = n - 1;
+		}
+	}
+}
