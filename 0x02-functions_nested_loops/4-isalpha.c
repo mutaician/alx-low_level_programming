b6d037236d30e02b089cfa9cb2560e0c12bd6084
Cyprian <cyprianmutai5900@gmail.com>
2023-02-21 06:50:51 -0800

    nested

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * Return: 1 if it is a letter,lowercase or uppercase and 0 for rest
  * @c: The characterin ASCII code
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if (c >= 65 && c <= 90 || c >= 97 && c >= 122)
 	{
