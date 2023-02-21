a7477cf78574ed0bec58b490f9dca485dbb09371
Cyprian <cyprianmutai5900@gmail.com>
2023-02-21 09:12:58 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,21 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n = n + 1;
 		}
 	}
-	else
+	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n = n - 1;
 		}
 	}
+	printf("%d\n", n);
 }
