d2c50d4802140571a0d8a52a6b96b62f1fb8e63d
Cyprian <cyprianmutai5900@gmail.com>
2023-02-21 07:44:41 -0800

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * print_last_digit: prints last digit
+ *
+ * @n: any integer
+ *
+ * Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n% 10;
+	if (l < 0);
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}
