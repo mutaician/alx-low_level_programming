93b6904db29d8671c8b124d3dfda8ea5972259b8
Cyprian <cyprianmutai5900@gmail.com>
2023-02-21 23:25:48 -0800

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - natural numbers
+ *
+ * Return: ALways 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
