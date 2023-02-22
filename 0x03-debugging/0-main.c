ccf3c9844ec04de96d7459c91c9253a3ab47c826
Cyprian <cyprianmutai5900@gmail.com>
2023-02-22 00:56:25 -0800

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
-/****
- * main - tests 0
+/**
+ * main -  tests functions if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
+	int i;
+
+	i = 0;
 	positive_or_negative(0);
+
 	return (0);
 }
