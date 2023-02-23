9c60d5486a9e99edff79f700d31e7fbb9aabe771
Cyprian <cyprianmutai5900@gmail.com>
2023-02-23 04:49:00 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * _isupper - prints upper case
+ * @c: any character
+ *
+ * Return: 1 if c is uppercase
+ *0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
