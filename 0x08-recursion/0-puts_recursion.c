17abe89911ba758257382f51ec2e78d9b3912159
Cyprian <cyprianmutai5900@gmail.com>
2023-03-08 09:28:49 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') /* base case: end of string */
-    {
-        _putchar('\n');
-        return;
-    }
+	if (*s == '\0') /* base case: end of string */
+	{
+		_putchar('\n');
+		return;
+	}
 
-    _putchar(*s); /* print current character */
-    _puts_recursion(s + 1); /* recursive call with next character */
+	_putchar(*s); /* print current character */
+	_puts_recursion(s + 1); /* recursive call with next character */
 }
