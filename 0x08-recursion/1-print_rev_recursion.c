d69d2c475db9cdd7113150e3e08d899096431946
Cyprian <cyprianmutai5900@gmail.com>
2023-03-08 09:32:31 -0800

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s != '\0') /* recursive case: end of string not reached yet */
-    {
-        _print_rev_recursion(s + 1); /* recursive call with next character */
-        _putchar(*s); /* print current character */
-    }
+	if (*s != '\0') /* recursive case: end of string not reached yet */
+	{
+		_print_rev_recursion(s + 1); /* recursive call with next character */
+		_putchar(*s); /* print current character */
+	}
 }
