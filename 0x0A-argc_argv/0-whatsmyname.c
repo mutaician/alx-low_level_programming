547b905d59d0bb3efe098a06c9d8e17966a24ded
Cyprian <cyprianmutai5900@gmail.com>
2023-03-10 08:15:54 -0800

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: arrar containing command line arguments
  * Return: 0
  */
-int mai(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
