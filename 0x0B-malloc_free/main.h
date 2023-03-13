595d0756cf2bb735f22d3a573d6299d88e11c632
Cyprian <cyprianmutai5900@gmail.com>
2023-03-13 06:12:45 -0700

    malloc

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,9 @@
 #define _main_h_
 
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create(unsigned int size, char c);
 
 #endif
